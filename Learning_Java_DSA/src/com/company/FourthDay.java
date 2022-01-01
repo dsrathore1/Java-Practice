05e4061f152da61beca171cd3403801817cb7352
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-01 12:19:33 +0530

    Learning DSA in JAVA

---
+++ Learning_Java_DSA/src/com/company/FourthDay.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FourthDay {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if (n % 2 == 0) {
+            if (n > 2 && n < 5) {
+                System.out.println("Not Weird");
+            } else if (n > 6 && n <= 20) {
+                System.out.println("Weird");
+            } else if (n > 20) {
+                System.out.println("Not Weird");
+            }
+        } else {
+            System.out.println("Weird");
+        }
+    }
+}
