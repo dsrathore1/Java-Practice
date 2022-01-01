dc86b259fa879a7dd96d5d9c6c9d5adaafb19033
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-01 19:00:25 +0530

    Fifth Day

---
+++ Learning_Java_DSA/src/com/company/_5FifthDay.java
@@ -0,0 +1,20 @@
+//Java Stdin and Stdout II
+
+package com.company;
+
+import java.util.Scanner;
+
+public class _5FifthDay {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.next();
+
+        System.out.println("my integer :- " + a);
+        System.out.println("my integer :- " + d);
+        System.out.println("my integer :- " + s);
+
+    }
+}
