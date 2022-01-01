af470101247ac9698e1323921789ddecb699c840
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-01 18:51:24 +0530

    Third Day

---
+++ Learning_Java_DSA/src/com/company/_3ThirdDay.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class _3ThirdDay {
+    public static void main(String[] args) {
+        // Patterns
+//       1 Print(*****
+//               *****
+//               *****
+//               *****)
+
+        for(int i = 0; i<= 3;i++){
+            for(int j = 0; j <=4;j++){
+                System.out.print("#");
+            }
+            System.out.println("#");
+        }
+
+
+    }
+}
