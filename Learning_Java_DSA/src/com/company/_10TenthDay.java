89216bc6e9431776472db695e8c466e6580c0f9d
DS Rathore <dsrathorebsw1234@gmail.com>
2022-01-15 20:04:52 +0530

    Tenth Day

---
+++ Learning_Java_DSA/src/com/company/_10TenthDay.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class _10TenthDay {
+    public static void main(String[] args) {
+        // Swap the array element
+        int[] arr = {1, 3, 5, 6, 35, 26, 64};
+        int index1 = 0;
+        int index2 = 6;
+        swap(arr, index1, index2);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
