be61126b5480fcca4c7a175c4a52f30ac132da38
DS Rathore <dsrathorebsw1234@gmail.com>
2022-08-27 12:23:52 +0530

    DSA

--- Questions/src/com/company/DuplicateElement.java
+++ Questions/src/com/company/DuplicateElement.java
@@ -1,2 +1,17 @@
-package com.company;public class DuplicateElement {
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicateElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 4, 2, 1, 4, 7,4};
+        Set<Integer> s = new HashSet<>();
+
+        for (int n : arr) {
+            if (!s.add(n)) {
+                System.out.print(n + " ");
+            }
+        }
+    }
 }
--- Questions/src/com/company/ReverseArray.java
+++ Questions/src/com/company/ReverseArray.java
@@ -1,2 +1,10 @@
-package com.company;public class ReverseArray {
+package com.company;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 22, 5, 24, 76, 46, 88, 43};
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
