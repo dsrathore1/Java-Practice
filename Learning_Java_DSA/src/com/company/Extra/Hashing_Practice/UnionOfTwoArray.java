e5b33fbbe9855c0654fb407f73fcb1e3e423c293
DS Rathore <dsrathorebsw1234@gmail.com>
2022-08-28 08:09:17 +0530

    Added Another way to merge two array

--- Learning_Java_DSA/src/com/company/Extra/Hashing_Practice/UnionOfTwoArray.java
+++ Learning_Java_DSA/src/com/company/Extra/Hashing_Practice/UnionOfTwoArray.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 public class UnionOfTwoArray {
     public static void mergeTheArray(int[] a, int[] b) {
         //! Add two array in one array
+        //! With Time Complexity :- O(M+N)
+
         int length = a.length + b.length;
         int[] result = new int[length];
 
@@ -28,13 +30,27 @@ public class UnionOfTwoArray {
         }
 
         System.out.print("\nAfter Union :- ");
-        System.out.print(hs + " ");
+        System.out.println(hs + " ");
+    }
+
+    public static void anotherWayToMerge(int[] a, int[] b) {
+        //! Another way to merge two array
+        int length2 = a.length + b.length;
+
+        int[] c = new int[length2];
+
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+
+        System.out.println(Arrays.toString(c));
     }
 
     public static void main(String[] args) {
         int[] a = {5, 10, 15, 5};
         int[] b = {10, 15, 4, 5};
 
-        mergeTheArray(a, b);
+        mergeTheArray(a, b); //! Time complexity :- O(M+N)
+        System.out.print("Another way to merge the two array :- "); //! Merge array using arraycopy
+        anotherWayToMerge(a, b); //! Time complexity :- O(M+N)
     }
 }
