37cffc4a369248f9a7687be8efe3ce6ec6c86f18
DS Rathore <dsrathorebsw1234@gmail.com>
2022-08-28 08:19:42 +0530

    Merge array using Hashset

--- Learning_Java_DSA/src/com/company/Extra/Hashing_Practice/UnionOfTwoArray.java
+++ Learning_Java_DSA/src/com/company/Extra/Hashing_Practice/UnionOfTwoArray.java
@@ -45,12 +45,29 @@ public class UnionOfTwoArray {
         System.out.println(Arrays.toString(c));
     }
 
+    //! Simple way to add two array using HashSet
+    //? NOTE :- No Duplicate value will add into it
+
+    public static void mergeWithHashset(int[] a, int[] b) {
+        HashSet<Integer> hs = new HashSet<>();
+
+        for (int j : a) {  //! For each Loop
+            hs.add(j);
+        }
+
+        for (int k : b) {
+            hs.add(k);
+        }
+        System.out.println("Your merged array :- " + hs + "\nThe size of the array is :- " + hs.size());
+    }
+
     public static void main(String[] args) {
-        int[] a = {5, 10, 15, 5};
-        int[] b = {10, 15, 4, 5};
+        int[] a = {5, 10, 15, 5, 44, 1, 10};
+        int[] b = {10, 15, 4, 5, 6, 2, 69};
 
         mergeTheArray(a, b); //! Time complexity :- O(M+N)
         System.out.print("Another way to merge the two array :- "); //! Merge array using arraycopy
         anotherWayToMerge(a, b); //! Time complexity :- O(M+N)
+        mergeWithHashset(a, b); //! Time complexity :- O(n^2)
     }
 }
